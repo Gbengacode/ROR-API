c59afa05ebfc7e47c15d6fb92c72461d6ec20851
emmanuel <gbengacode>
2022-12-10 23:24:51 +0100

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_192416) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_180803) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
